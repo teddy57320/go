34b451c63c740f165b46d7c7c88ebcdc0db0d88f
Ted Huang <teddy57320@gmail.com>
2019-12-25 13:26:32 -0800

    implement Board class inheriting from np.ndarray, some Board methods

--- board.py
+++ board.py
@@ -1,29 +1,57 @@
 import numpy as np
 from utils import Stone
 
-class Board(object):
+class Board(np.ndarray):
 
-    def __init__(self, config):
-        self.board_size = config['board_size']
-        self.black_stone_render = config['black_stone']
-        self.white_stone_render = config['white_Stone']
-        self.board = np.zeros((self.board_size, self.board_size))
+    def __new__(cls, config={}):
+
+        board_size = config['board_size']
+        shape = (board_size, board_size)
+        obj = super(Board, cls).__new__(cls, shape, dtype=np.int)
+
+        obj.board_size = board_size
+        obj.black_stone_render = config['black_stone']
+        obj.white_stone_render = config['white_stone']
+        obj.moves = []
+
+        obj.fill(Stone.EMPTY)
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.board_size = getattr(obj, 'board_size')
+        self.black_stone_render = getattr(obj, 'black_stone_render')
+        self.white_stone_render = getattr(obj, 'white_stone_render')
+        self.moves = getattr(obj, 'moves')
 
     def place_black(self, y, x):
-        self.board[y][x] = Stone.BLACK
+        self._place_stone(Stone.BLACK, y, x)
 
     def place_white(self, y, x):
-        self.board[y][x] = Stone.WHITE
+        self._place_stone(Stone.WHITE, y, x)
 
-    def value_to_printed(self, x):
+    def _place_stone(self, stone, y, x):
+        self[y][x] = stone
+        self.moves.append((y, x))
+
+    def undo_last_move(self):
+        if not self.moves:
+            return
+        y, x = self.moves.pop()
+        self[y][x] = Stone.EMPTY
+
+    def _value_to_render(self, x):
+        s = None
         if x == Stone.EMPTY:
-            return ' '
+            s = ' '
         if x == Stone.BLACK:
-            return self.black_stone_render
+            s = self.black_stone_render
         if x == Stone.WHITE:
-            return self.white_stone_render
+            s = self.white_stone_render
+        return f'[{s}]'
 
     def render_board(self):
         for row in range(self.board_size):
-            board_row = map(self.value_to_printed, self.board[row])
-            print(board_row)
+            board_row = map(self._value_to_render, self[row])
+            print(''.join(board_row))
--- game.py
+++ game.py
@@ -1,2 +1,15 @@
+from board import Board
+
 class Game(object):
-    pass
+    def __init__(self, config):
+        self.board = Board(config)
+        self.board_size = config['board_size']
+
+    def _check_win(self, y, x):
+        '''
+        To check if a piece placed at (y, x) results in a win, we only need to 
+        check the 9x9 pixel patch centered at (y, x)
+        '''
+        window = 9
+        # todo
+
